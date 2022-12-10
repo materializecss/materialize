318a5284643421ea513a355d3ce6735ed76ec7de
Mauro Mascarenhas de Araújo <mauromascarenhas@users.noreply.github.com>
2022-12-10 13:42:02 -0300

    test(carousel): carousel accessibility
    Implement carousel test cases:
    
    - Update HTML test files:
        - Add alternative tests for images;
        - Add indicators and indicatorLabelFunc to carousel instances.
    - Add test cases to js spec file:
        - Label indicator check;
        - Non-current Full Width slides must be hidden.

--- tests/spec/carousel/carouselSpec.js
+++ tests/spec/carousel/carouselSpec.js
@@ -1,34 +1,60 @@
-describe("Carousel", function () {
+describe("Carousel", () => {
 
-  beforeEach(async function() {
+  beforeEach(async () => {
     await XloadFixtures(['carousel/carouselFixture.html']);
   });
-  afterEach(function(){
+
+  afterEach(() => {
     XunloadFixtures();
   });
 
-  describe("carousel plugin", function () {
+  describe("carousel plugin", () => {
 
-    // beforeEach(function() {
-    // });
-
-    it("No wrap next and prev should not overflow", function (done) {
+    it("No wrap next and prev should not overflow", (done) => {
       let noWrap = M.Carousel.init(
-        document.querySelector('#slider-no-wrap'), { noWrap: true }
+        document.querySelector("#slider-no-wrap"), { noWrap: true }
       );
       noWrap.prev();
 
-      expect(noWrap.center).toEqual(0, 'Prev should do nothing');
+      expect(noWrap.center).toEqual(0, "Prev should do nothing");
 
       noWrap.set(3);
-      setTimeout(function() {
+      setTimeout(() => {
         noWrap.next();
 
-        setTimeout(function() {
-          expect(noWrap.center).toEqual(3, 'Next should do nothing');
+        setTimeout(() => {
+          expect(noWrap.center).toEqual(3, "Next should do nothing");
+          noWrap.destroy();
 
           done();
         }, 400);
+      }, 400);
+    });
+
+    it("Label of indicators must start with 'Slide '", () => {
+      let carousel = M.Carousel.init(
+        document.querySelector("#slider-no-wrap"), {
+          indicators: true,
+          indicatorLabelFunc: (idx) => "Slide " + idx
+        }
+      );
+
+      expect(Array.from(document.querySelectorAll("button")).map((btn) =>
+        btn.getAttribute("aria-label").startsWith("Slide ")
+      )).toEqual([true, true, true, true]);
+      carousel.destroy();
+    });
+
+    it("Non-current slides must be hidden (fullWidth)", (done) => {
+      let carousel = M.Carousel.init(document.querySelector("#slider-no-wrap"), { fullWidth: true });
+
+      setTimeout(() => {
+        expect(Array.from(document.querySelectorAll(".carousel-item")).map((item) => item.style.visibility))
+          .toEqual(["visible", "", "", ""]);
+        expect(Array.from(document.querySelectorAll(".carousel-item")).map((item) => item.getAttribute("aria-hidden")))
+          .toEqual(["false", "true", "true", "true"]);
+        carousel.destroy();
+        done();
       }, 400);
     });
 
