41a88240afc1bd3ae5382d42471bd74244fa9cba
Daniel Wurzer <wurzer.da@gmail.com>
2023-12-19 01:56:03 +0100

    chore: release 2.0.3

--- src/global.ts
+++ src/global.ts
@@ -26,7 +26,7 @@ import { Range } from './range';
 import { Utils } from './utils';
 
 export class M {
-  static version = '2.0.3-beta';
+  static version = '2.0.3';
 
   static Autocomplete: typeof Autocomplete = Autocomplete;
   static Tabs: typeof Tabs = Tabs;
