617c8e8e9f65c4d6039e25eb6ebc70ae079eb6a6
Geert Selderslaghs <geert@codemindit.com>
2024-11-28 05:39:00 +0100

    (enhancement) removed pretier config for now

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,4 @@
 import typescriptEslint from 'typescript-eslint';
-import prettierPlugin from 'eslint-plugin-prettier';
-import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
   {
@@ -9,27 +7,17 @@ export default [
   },
   ...typescriptEslint.configs.recommended,
   {
-    plugins: {
-      prettier: prettierPlugin
-    },
     rules: {
-      ...prettierPlugin.configs.recommended.rules,
-      ...eslintConfigPrettier.rules,
-
-      // TODO: to improve because the basic rules cause
-      // thousands of errors, for this reason, they have been marked as WARN
-      'prettier/prettier': 'warn',
-
-      'prefer-const': 'warn',
+      'prefer-const': 'error',
       'prefer-rest-params': 'warn',
       'no-var': 'warn',
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/no-unused-vars': 'warn',
-      '@typescript-eslint/no-unused-expressions': 'warn',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-unused-expressions': 'error',
       '@typescript-eslint/no-this-alias': 'warn',
-      '@typescript-eslint/no-empty-object-type': 'warn',
-      '@typescript-eslint/no-require-imports': 'warn',
-      '@typescript-eslint/no-unsafe-function-type': 'warn'
+      '@typescript-eslint/no-empty-object-type': 'error',
+      '@typescript-eslint/no-require-imports': 'error',
+      '@typescript-eslint/no-unsafe-function-type': 'error'
     }
   }
 ];
