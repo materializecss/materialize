cc2fde5ffe2e0648312e43665fde8b1b9f8ed89d
Daniel Wurzer <info@wuda.io>
2024-12-16 09:25:53 +0100

    refactor: remove experimental stuff

--- index.js
+++
@@ -1,6 +0,0 @@
-const { Button, Grid } = require('@materializecss/materialize');
-
-// This is experimental
-
-const html = Grid(['<p>Are you cool?</p>', Button('Yes'), Button('No')].join(''));
-console.log(html);
