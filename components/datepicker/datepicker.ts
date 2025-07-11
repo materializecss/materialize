b86132aac44343b2bfb489cc336e6a8ce866ecb8
Geert Selderslaghs <geert@codemindit.com>
2025-07-11 22:55:47 +0200

    Revert "fix(Datepicker) fixes bug where modal closes on calendar interaction"
    This reverts commit 849ae54adc016aa34c6d92c9633602d50ab27251.

--- components/datepicker/datepicker.ts
+++ components/datepicker/datepicker.ts
@@ -1093,6 +1093,7 @@ export class Datepicker extends Component<DatepickerOptions> {
       maxYear = opts.maxYear,
       minMonth = opts.minMonth,
       maxMonth = opts.maxMonth;
+    let html = '';
 
     if (this._y <= minYear) {
       this._y = minYear;
@@ -1107,19 +1108,12 @@ export class Datepicker extends Component<DatepickerOptions> {
       }
     }
 
-    setTimeout(() => {
-      this.drawHTML();
-    }, 10);
-  }
-
-  drawHTML() {
     const randId =
       'datepicker-title-' +
       Math.random()
         .toString(36)
         .replace(/[^a-z]+/g, '')
         .substr(0, 2);
-    let html = '';
 
     for (let c = 0; c < 1; c++) {
       if (!this.options.isDateRange) {
