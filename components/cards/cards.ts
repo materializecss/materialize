cf49ca7a308e7518255020c4a2c445c055ad445c
Daniel Wurzer <info@wuda.io>
2026-01-02 14:40:43 +0100

    refactor: use native JS private features

--- components/cards/cards.ts
+++ components/cards/cards.ts
@@ -1,7 +1,7 @@
 import { Utils } from '../../src/utils';
 import { Component, BaseOptions, InitElements, MElement, Openable } from '../../src/component';
 
-export interface CardsOptions extends BaseOptions {
+interface CardsOptions extends BaseOptions {
   onOpen: (el: Element) => void;
   onClose: (el: Element) => void;
   inDuration: number;
@@ -15,37 +15,32 @@ const _defaults: CardsOptions = {
   outDuration: 300
 };
 
-export class Cards extends Component<CardsOptions> implements Openable {
+class Cards extends Component<CardsOptions> implements Openable {
+  readonly #cardReveal: HTMLElement | null;
+  readonly #initialOverflow: string;
+  #activators: HTMLElement[] | null;
+  #cardRevealClose: HTMLElement | null;
   isOpen: boolean = false;
-  private readonly cardReveal: HTMLElement | null;
-  private readonly initialOverflow: string;
-  private _activators: HTMLElement[] | null;
-  private cardRevealClose: HTMLElement | null;
 
   constructor(el: HTMLElement, options: Partial<CardsOptions>) {
     super(el, options, Cards);
     this.el['M_Cards'] = this;
-
     this.options = {
       ...Cards.defaults,
       ...options
     };
-
-    this._activators = [];
-
-    this.cardReveal = this.el.querySelector('.card-reveal');
-    if (this.cardReveal) {
-      this.initialOverflow = getComputedStyle(this.el).overflow;
-      this._activators = Array.from(this.el.querySelectorAll('.activator'));
-      this._activators.forEach((el: HTMLElement) => {
+    this.#activators = [];
+    this.#cardReveal = this.el.querySelector('.card-reveal');
+    if (this.#cardReveal) {
+      this.#initialOverflow = getComputedStyle(this.el).overflow;
+      this.#activators = Array.from(this.el.querySelectorAll('.activator'));
+      this.#activators.forEach((el: HTMLElement) => {
         if (el) el.tabIndex = 0;
       });
-
-      this.cardRevealClose = this.cardReveal?.querySelector('.card-title');
-      if (this.cardRevealClose) this.cardRevealClose.tabIndex = -1;
-
-      this.cardReveal.ariaExpanded = 'false';
-      this._setupEventHandlers();
+      this.#cardRevealClose = this.#cardReveal?.querySelector('.card-title');
+      if (this.#cardRevealClose) this.#cardRevealClose.tabIndex = -1;
+      this.#cardReveal.ariaExpanded = 'false';
+      this.#setupEventHandlers();
     }
   }
 
@@ -85,51 +80,50 @@ export class Cards extends Component<CardsOptions> implements Openable {
    * {@inheritDoc}
    */
   destroy() {
-    this._removeEventHandlers();
-    this._activators = [];
+    this.#removeEventHandlers();
+    this.#activators = [];
   }
 
-  _setupEventHandlers = () => {
-    this._activators.forEach((el: HTMLElement) => {
-      el.addEventListener('click', this._handleClickInteraction);
-      el.addEventListener('keypress', this._handleKeypressEvent);
+  #setupEventHandlers = () => {
+    this.#activators.forEach((el: HTMLElement) => {
+      el.addEventListener('click', this.#handleClickInteraction);
+      el.addEventListener('keypress', this.#handleKeypressEvent);
     });
   };
 
-  _removeEventHandlers = () => {
-    this._activators.forEach((el: HTMLElement) => {
-      el.removeEventListener('click', this._handleClickInteraction);
-      el.removeEventListener('keypress', this._handleKeypressEvent);
+  #removeEventHandlers = () => {
+    this.#activators.forEach((el: HTMLElement) => {
+      el.removeEventListener('click', this.#handleClickInteraction);
+      el.removeEventListener('keypress', this.#handleKeypressEvent);
     });
   };
 
-  _handleClickInteraction = () => {
-    this._handleRevealEvent();
+  #handleClickInteraction = () => {
+    this.#handleRevealEvent();
   };
 
-  _handleKeypressEvent: (e: KeyboardEvent) => void = (e: KeyboardEvent) => {
+  #handleKeypressEvent: (e: KeyboardEvent) => void = (e: KeyboardEvent) => {
     if (Utils.keys.ENTER.includes(e.key)) {
-      this._handleRevealEvent();
+      this.#handleRevealEvent();
     }
   };
 
-  _handleRevealEvent = () => {
-    // Reveal Card
-    this._activators.forEach((el: HTMLElement) => (el.tabIndex = -1));
+  #handleRevealEvent = () => {
+    this.#activators.forEach((el: HTMLElement) => (el.tabIndex = -1)); // Reveal Card
     this.open();
   };
 
-  _setupRevealCloseEventHandlers = () => {
-    this.cardRevealClose.addEventListener('click', this.close);
-    this.cardRevealClose.addEventListener('keypress', this._handleKeypressCloseEvent);
+  #setupRevealCloseEventHandlers = () => {
+    this.#cardRevealClose.addEventListener('click', this.close);
+    this.#cardRevealClose.addEventListener('keypress', this.#handleKeypressCloseEvent);
   };
 
-  _removeRevealCloseEventHandlers = () => {
-    this.cardRevealClose.addEventListener('click', this.close);
-    this.cardRevealClose.addEventListener('keypress', this._handleKeypressCloseEvent);
+  #removeRevealCloseEventHandlers = () => {
+    this.#cardRevealClose.addEventListener('click', this.close);
+    this.#cardRevealClose.addEventListener('keypress', this.#handleKeypressCloseEvent);
   };
 
-  _handleKeypressCloseEvent: (e: KeyboardEvent) => void = (e: KeyboardEvent) => {
+  #handleKeypressCloseEvent: (e: KeyboardEvent) => void = (e: KeyboardEvent) => {
     if (Utils.keys.ENTER.includes(e.key)) {
       this.close();
     }
@@ -142,17 +136,17 @@ export class Cards extends Component<CardsOptions> implements Openable {
     if (this.isOpen) return;
     this.isOpen = true;
     this.el.style.overflow = 'hidden';
-    this.cardReveal.style.display = 'block';
-    this.cardReveal.ariaExpanded = 'true';
-    this.cardRevealClose.tabIndex = 0;
+    this.#cardReveal.style.display = 'block';
+    this.#cardReveal.ariaExpanded = 'true';
+    this.#cardRevealClose.tabIndex = 0;
     setTimeout(() => {
-      this.cardReveal.style.transition = `transform ${this.options.outDuration}ms ease`; //easeInOutQuad
-      this.cardReveal.style.transform = 'translateY(-100%)';
+      this.#cardReveal.style.transition = `transform ${this.options.outDuration}ms ease`; //easeInOutQuad
+      this.#cardReveal.style.transform = 'translateY(-100%)';
     }, 1);
     if (typeof this.options.onOpen === 'function') {
       this.options.onOpen.call(this);
     }
-    this._setupRevealCloseEventHandlers();
+    this.#setupRevealCloseEventHandlers();
   };
 
   /**
@@ -161,19 +155,19 @@ export class Cards extends Component<CardsOptions> implements Openable {
   close: () => void = () => {
     if (!this.isOpen) return;
     this.isOpen = false;
-    this.cardReveal.style.transition = `transform ${this.options.inDuration}ms ease`; //easeInOutQuad
-    this.cardReveal.style.transform = 'translateY(0)';
+    this.#cardReveal.style.transition = `transform ${this.options.inDuration}ms ease`; //easeInOutQuad
+    this.#cardReveal.style.transform = 'translateY(0)';
     setTimeout(() => {
-      this.cardReveal.style.display = 'none';
-      this.cardReveal.ariaExpanded = 'false';
-      this._activators.forEach((el: HTMLElement) => (el.tabIndex = 0));
-      this.cardRevealClose.tabIndex = -1;
-      this.el.style.overflow = this.initialOverflow;
+      this.#cardReveal.style.display = 'none';
+      this.#cardReveal.ariaExpanded = 'false';
+      this.#activators.forEach((el: HTMLElement) => (el.tabIndex = 0));
+      this.#cardRevealClose.tabIndex = -1;
+      this.el.style.overflow = this.#initialOverflow;
     }, this.options.inDuration);
     if (typeof this.options.onClose === 'function') {
       this.options.onClose.call(this);
     }
-    this._removeRevealCloseEventHandlers();
+    this.#removeRevealCloseEventHandlers();
   };
 
   static Init() {
@@ -187,3 +181,5 @@ export class Cards extends Component<CardsOptions> implements Openable {
       });
   }
 }
+
+export { Cards, CardsOptions };
