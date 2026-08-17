2c998165c1d33639724e09a2c713a419bc41adad
Daniel Wurzer <info@wuda.io>
2026-08-17 11:47:10 +0200

    fix: appbar

--- components/appbar/appbar.mjs
+++ components/appbar/appbar.mjs
@@ -3,34 +3,20 @@ import { Component } from '../atomic/component.mjs';
 // AppBar is the old "Navbar"
 
 class AppBar extends Component {
-  #title = '';
+  #title;
   #items;
 
+  /**
+   *
+   * @param {{isFixed: Boolean = false}} options
+   */
   constructor(options) {
     super(options);
+    this.#title = '';
     this.#items = [];
-    this.setTagName('nav').addClassname('nav navbar');
-  }
-
-  setTitle(title) {
-    this.#title = title;
-    return this;
-  }
-
-  setSearch() {
-    return this;
-  }
-
-  setLogo() {
-    return this;
-  }
-
-  addLeftIcon() {
-    return this;
-  }
-
-  addRightIcon() {
-    return this;
+    if (options.isFixed) {
+      this.addClassname('navbar-fixed');
+    }
   }
 
   addItem(item) {
@@ -38,24 +24,42 @@ class AppBar extends Component {
     return this;
   }
 
+  //////////////////////// unstable api atm
+
+  setTitle(title) {
+    this.#title = title;
+    return this;
+  }
+  setSearch() {
+    return this;
+  }
+  setLogo() {
+    return this;
+  }
+  addLeftIcon() {
+    return this;
+  }
+  addRightIcon() {
+    return this;
+  }
+
+  //
+
   toHTML() {
-    const html = `<div class="nav-wrapper">
-      <a href="#" class="brand-logo">${this.#title}</a>
-
-      <ul class="right hide-on-med-and-down">
-        <li><a href="sass.html"><i class="material-icons">search</i></a></li>
-        <li><a href="badges.html"><i class="material-icons">view_module</i></a></li>
-        <li><a href="collapsible.html"><i class="material-icons">refresh</i></a></li>
-        <li><a href="mobile.html"><i class="material-icons">more_vert</i></a></li>
-        ${this.#items.map((item) => `<li>${item}</li>`).join('')}
-      </ul>
-
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
-      </ul>
-    </div>`;
+    const html = `<nav class="nav navbar">
+      <div class="nav-wrapper">
+        ${this.#title}
+        <ul class="right hide-on-med-and-down">
+          <!--
+          <li><a href="sass.html"><i class="material-icons">search</i></a></li>
+          <li><a href="badges.html"><i class="material-icons">view_module</i></a></li>
+          <li><a href="collapsible.html"><i class="material-icons">refresh</i></a></li>
+          <li><a href="mobile.html"><i class="material-icons">more_vert</i></a></li>
+          -->
+          ${this.#items.map((item) => `<li>${item}</li>`).join('')}
+        </ul>
+      </div>
+    </nav`;
     this.setChildren(html);
     return super.toHTML();
   }
