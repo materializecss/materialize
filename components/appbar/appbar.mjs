20410bd8af78e74519b9a2f63bf4b361a0cfbbb7
Daniel Wurzer <info@wuda.io>
2026-09-17 12:34:14 +0200

    fix(appbar): content

--- components/appbar/appbar.mjs
+++ components/appbar/appbar.mjs
@@ -8,7 +8,7 @@ class AppBar extends Component {
 
   /**
    *
-   * @param {{isFixed: Boolean = false, hasContainer: Boolean = true}} options
+   * @param {{isFixed: Boolean = false, hasContainer: Boolean = true, content: any}} options
    */
   constructor(options) {
     super(options);
@@ -47,18 +47,13 @@ class AppBar extends Component {
   //
 
   toHTML() {
-    /*
-    <li><a href="sass.html"><i class="material-icons">search</i></a></li>
-    <li><a href="badges.html"><i class="material-icons">view_module</i></a></li>
-    <li><a href="collapsible.html"><i class="material-icons">refresh</i></a></li>
-    <li><a href="mobile.html"><i class="material-icons">more_vert</i></a></li>
-    */
     const html = `<nav class="nav navbar ${this.options.hasContainer ? 'container' : ''}">
       <div class="nav-wrapper">
         ${this.#title}
         <ul class="hide-on-med-and-down">
           ${this.#items.map((item) => `<li>${item}</li>`).join('')}
         </ul>
+        ${this.options.content || ''}
       </div>
     </nav>`;
     this.setChildren(html);
