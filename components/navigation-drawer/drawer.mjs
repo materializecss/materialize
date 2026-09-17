6484cb427fd8db03de76201a188eb4e709e21de8
Daniel Wurzer <info@wuda.io>
2026-09-17 14:22:24 +0200

    fix(drawer): remove hardcoded stuff

--- components/navigation-drawer/drawer.mjs
+++ components/navigation-drawer/drawer.mjs
@@ -18,14 +18,14 @@ class Drawer extends Component {
   }
 
   toHTML() {
-    const html =
-      `<li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
-      <li><a href="#!">Second Link</a></li>
-      <li><div class="divider"></div></li>
-      <li><a class="subheader">Subheader</a></li>
-      <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>` +
-      this.#items.map((item) => `<li>${item}</li>`).join('');
-    // <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+    /*
+    <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
+    <li><a href="#!">Second Link</a></li>
+    <li><div class="divider"></div></li>
+    <li><a class="subheader">Subheader</a></li>
+    <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+    */
+    const html = this.#items.map((item) => `<li>${item}</li>`).join('');
     this.setChildren(html);
     return super.toHTML();
   }
